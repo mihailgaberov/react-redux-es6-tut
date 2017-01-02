666c5738a6d26e6b1472abc99f00346cbaaf3599
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-01-02 13:06:18 +0100

    Finished Production Builds - Added Store configuration for production. Added webpack config for production. Added build scripts. Added distServer for serving the app in production mode. Ammended the package.json to include the build process.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-
-export default function configureStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
-  );
+/**
+ * Created by Mihail on 1/2/2017.
+ */
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod');
+} else {
+  module.exports = require('./configureStore.dev');
 }
